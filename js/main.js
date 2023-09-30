806d799d26096ebc0a5d73e722b57f42afd265d8
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-09-29 22:44:03 -0600

    Copiar contructor objeto Cita

--- js/main.js
+++ js/main.js
@@ -7,3 +7,58 @@ $('#datepicker input').datepicker({
     autoclose: true,
     todayHighlight: true
 })
+
+class Cita {
+    constructor(nombre, anio, mes, dia, hora) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.mes = mes;
+        this.dia = dia;
+        this.hora = hora;
+        this.fecha = new Date(this.anio, (this.mes - 1), this.dia, this.hora).toJSON(); //se agrega Date() por si se ocupa después
+    }
+}
+
+function validateCalendar(calendario, cita) {
+    const duplicado = calendario.filter((obj) => obj.anio === cita.anio && obj.mes === cita.mes && obj.dia === cita.dia && obj.hora === cita.hora);
+    return duplicado.length > 0;
+}
+
+//Citas que ya fueron registradas
+const calendario = [{
+    "nombre": "Modesto Arreola",
+    "anio": 2023,
+    "mes": 1,
+    "dia": 1,
+    "hora": 10,
+    "fecha": "2023-01-01T16:00:00.000Z"
+}]
+
+let cita
+while (true) {
+    cita = form();
+
+    if (validateCalendar(calendario, cita)) {
+        alert('Cita Duplicada')
+    } else {
+        calendario.push(cita)
+        console.log("Tu cita es para el " + cita.dia + "/" + cita.mes + "/" + cita.anio + " a las " + cita.hora + ":00 horas");
+        console.log("A nombre de " + cita.nombre);
+        break;
+    }
+
+}
+
+const config = {
+    name: "Cita para plan nutricional",
+    description: "Esta es tu cita para tu plan nutricional y asesoría, recuerda contar con los datos que se te pide",
+    startDate: cita.anio + "-" + cita.mes + "-" + cita.dia,
+    startTime: cita.hora + ":00",
+    endTime: (cita.hora + 1) + ":00",
+    options: ["Apple", "Google", "Outlook.com", "Yahoo"],
+    timeZone: "America/Mexico_City"
+};
+const button = document.getElementById('botonCalendario');
+if (button) {
+    button.addEventListener('click', () => atcb_action(config, button));
+}
