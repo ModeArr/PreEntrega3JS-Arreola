2c5259c54859541c88f208a46fb3efc97ab873fc
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-09-29 22:37:11 -0600

    FrontEnd

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#datepicker input').datepicker({
+    maxViewMode: 2,
+    todayBtn: true,
+    language: "es",
+    daysOfWeekDisabled: "0,6",
+    daysOfWeekHighlighted: "1,2,3,4,5",
+    autoclose: true,
+    todayHighlight: true
+})
