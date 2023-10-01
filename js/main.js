88f3161b76d9698860fe702fde3c5d0f0c5aa32c
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-09-30 19:51:50 -0600

    logia citas

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ $('#datepicker input').datepicker({
     todayHighlight: true
 })
 
+
+let citasAgregadas = document.getElementById("citaAgregada");
+if (!localStorage.getItem("cita")){
+    cita = JSON.parse(localStorage.getItem("cita"));
+    agregarCitaAlDom();
+}
+
 class Cita {
     constructor(nombre, anio, mes, dia, hora) {
         this.nombre = nombre;
@@ -15,13 +22,65 @@ class Cita {
         this.mes = mes;
         this.dia = dia;
         this.hora = hora;
-        this.fecha = new Date(this.anio, (this.mes - 1), this.dia, this.hora).toJSON(); //se agrega Date() por si se ocupa después
+        this.fecha= new Date(this.anio, (this.mes - 1), this.dia, this.hora).toJSON(); //se agrega Date() por si se ocupa después
+        this.fechaCreacion = Date();
     }
 }
 
 function validateCalendar(calendario, cita) {
     const duplicado = calendario.filter((obj) => obj.anio === cita.anio && obj.mes === cita.mes && obj.dia === cita.dia && obj.hora === cita.hora);
     return duplicado.length > 0;
+}
+
+let formaCita = document.getElementById("formaCita");
+formaCita.addEventListener("submit", (e) => {
+    if (!formaCita.checkValidity()) {
+        e.preventDefault()
+        e.stopPropagation()
+    
+    }else{
+        let nombre = document.getElementById("nombre").value;
+    let fechatest = document.getElementById("fecha").value;
+    let fechaCompleta = document.getElementById("fecha").value.split("/");
+    let anio = fechaCompleta[2];
+    let mes = fechaCompleta[1];
+    let dia = fechaCompleta[0]; 
+    let hora = document.getElementById("floatingSelectGrid").value;
+    const cita = new Cita(nombre, anio, mes, dia, hora);
+
+    if (!validateCalendar(calendario, cita)){
+        guardarCita(cita);
+        calendario.push(cita);
+        agregarCitaAlDom(cita);
+    }
+    }
+    
+    formaCita.classList.add('was-validated')
+})
+
+function borrarCita(){
+    localStorage.removeItem('cita');
+    citasAgregadas.remove();
+}
+
+function guardarCita(cita){
+    localStorage.setItem("cita", JSON.stringify(cita))
+}
+
+function agregarCitaAlDom(cita){
+    citasAgregadas.innerHTML = `<div class="card text-center">
+    <div class="card-header">
+      Cita
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Agregaste una cita ${cita?.nombre}</h5>
+      <p class="card-text">Tu cita es para el ${cita?.dia}/${cita?.mes}/${cita?.anio} a las ${cita?.hora}:00 horas</p>
+      <a onclick="borrarCita()" class="btn btn-primary">Cancelar cita</a>
+    </div>
+    <div class="card-footer text-body-secondary">
+      Recuerda llegar 5 minutos antes a tu cita.
+    </div>
+  </div>`
 }
 
 //Citas que ya fueron registradas
@@ -34,22 +93,7 @@ const calendario = [{
     "fecha": "2023-01-01T16:00:00.000Z"
 }]
 
-let cita
-while (true) {
-    cita = form();
-
-    if (validateCalendar(calendario, cita)) {
-        alert('Cita Duplicada')
-    } else {
-        calendario.push(cita)
-        console.log("Tu cita es para el " + cita.dia + "/" + cita.mes + "/" + cita.anio + " a las " + cita.hora + ":00 horas");
-        console.log("A nombre de " + cita.nombre);
-        break;
-    }
-
-}
-
-const config = {
+/* const config = {
     name: "Cita para plan nutricional",
     description: "Esta es tu cita para tu plan nutricional y asesoría, recuerda contar con los datos que se te pide",
     startDate: cita.anio + "-" + cita.mes + "-" + cita.dia,
@@ -61,4 +105,4 @@ const config = {
 const button = document.getElementById('botonCalendario');
 if (button) {
     button.addEventListener('click', () => atcb_action(config, button));
-}
+} */
